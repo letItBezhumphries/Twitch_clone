57a2463ba60b8aa05e386313b6acfdbe9419d377
Eric Humphries <ezhumphries@gmail.com>
2020-04-24 13:49:05 -0700

    added client react app with basic pages/components

---
+++ streams/client/src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="ui secondary pointing menu">
+      <Link to="/" className="item">
+        Streamy
+      </Link>
+      <div className="right menu">
+        <Link to="/" className="item">
+          All Streams
+        </Link>
+        <Link to="/login" className="item">
+          Login
+        </Link>
+      </div>
+      Header
+    </div>
+  );
+};
+
+export default Header;
